f966d7e653bd29431892d9337b394fdc63d2a5ae
MohJamal <mohjamalalkhatib96@gmail.com>
2025-08-18 23:41:20 +0300

    11 - Controllers in CodeIgniter Part 2 => Remapping Method Calls + Private methods using 'private' modifiers or prefixing method names with an underscore + Reserved Names like show_404() function and ENVIRONMENT constant.

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,23 +3,55 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Test extends CI_Controller
 {
-    public function index()
+    private function myFun()
     {
-        echo 'This is the test index';
+        echo 'Hello This is MyFun';
     }
 
-    public function welcome()
+    // public function _myFun()
+    // {
+    //     echo 'Hello This is MyFun';
+    // }
+
+    public function _remap($action)
     {
-        echo 'Welcome to CodeIgniter';
+        if ($action === "greeting") {
+            $this->greeting();
+        } else if ($action === "index") {
+            $this->index();
+        } else {
+            $this->defaultMethod();
+        }
+    }
+
+    public function index()
+    {
+        // show_404();
+        // echo VIEWPATH;
+        // echo FCPATH;
+        // echo ENVIRONMENT;
+        // echo 'This is the test index';
+    }
+
+    public function welcome($name)
+    {
+        // echo 'Welcome to CodeIgniter';
+        print_r($name);
     }
 
     public function greeting()
     {
+        $this->myFun();
         echo 'Greetings for You';
     }
 
     function edit($id)
     {
         echo $id;
+    }
+
+    function defaultMethod()
+    {
+        echo "Sorry! Not able to process";
     }
 }
